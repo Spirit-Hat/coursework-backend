5d64c6dec814cafa9995a2879180e20ed1d87e01
Spirit-Hat <vladonline34@gmail.com>
2022-06-17 15:20:06 +0300

    Initial commit

---
+++ src/main/java/com/example/courseworkbackend/CourseworkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseworkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseworkBackendApplication.class, args);
+	}
+
+}
